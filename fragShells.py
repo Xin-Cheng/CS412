f53328d79680f8974ff3d4fe8284a74462662ca4
Xin-Cheng <x_cheng@outlook.com>
2017-03-05 04:53:33 -0800

    initial checkin

---
+++ fragShells.py
@@ -0,0 +1,15 @@
+# def buildIndexTable(datacube, partitions):
+#     return
+
+def main():
+    datacube = [
+        ['a1', 'b2', 'c1', 'd1', 'e1'],
+        ['a1', 'b2', 'c1', 'd2', 'e1'],
+        ['a1', 'b2', 'c1', 'd1', 'e2'],
+        ['a2', 'b1', 'c1', 'd1', 'e2'],
+        ['a2', 'b1', 'c1', 'd1', 'e3']
+    ]
+    partitions = 2
+
+if __name__ == "__main__":
+   main()
