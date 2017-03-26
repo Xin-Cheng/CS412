c2261fa75b801e27c2cdd0d335fc4ee8d9e0697d
Xin-Cheng <x_cheng@outlook.com>
2017-03-26 15:48:41 -0700

    sort and print

--- xcheng11-hw3/Apriori.py
+++ xcheng11-hw3/Apriori.py
@@ -8,18 +8,50 @@ def contains_set(transaction, item):
             return False
     return True
 
+# self join
+def self_join(new_set):
+    if len(new_set) <= 1:
+        return new_set
+    itemset = []
+    for i in range(len(new_set)):
+        for j in range(i + 1, len(new_set)):
+            s = [ 0 ] + new_set[i][1:] + new_set[j][1:]
+            itemset.append(s)
+    return itemset
+
+# print result
+def print_patterns(freq_set):
+    def setCmp(s1, s2):
+        if s1[0] != s2[0]:
+            return s2[0] - s1[0]
+        else:
+            str1 = " ".join(s1[1:])
+            str2 = " ".join(s2[1:])
+            if str1 < str2:
+                return -1
+            else:
+                return 1
+    ordered_set = sorted(freq_set, cmp = setCmp)
+    for i in range(len(ordered_set)):
+        print ("{0} [{1}]").format(ordered_set[i][0], " ".join(ordered_set[i][1:]))
+    print ("finish")
+
 def apriori(dataset, itemset, min_sup, freq_set):
+    if (len(itemset) == 0):
+        return
     # count item
     for i in range(len(itemset)):
         for t in range(len(dataset)):
             if contains_set(dataset[t], itemset[i]):
                 itemset[i][0] += 1
     new_set = []
+    # prune
     for i in range(len(itemset)):
-        if itemset[i][0] >= min_sup:
-            new_set.append(itemset[i])
+        if itemset[i][0] >= min_sup:   
             freq_set.append(itemset[i])
-    print("apriori")
+            new_set.append(itemset[i])
+    new_itemset = self_join(new_set)
+    apriori(dataset, new_itemset, min_sup, freq_set)
 
 def main():
     dataset = []
@@ -38,6 +70,7 @@ def main():
         itemset.append(s)
     freq_set = []
     apriori(dataset, itemset, min_sup, freq_set)
+    print_patterns(freq_set)
     print ("finish")
 
 if __name__ == "__main__":
