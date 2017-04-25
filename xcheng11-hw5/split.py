5bae529928eadc5744deeabe5d053c6035964543
freddie <x_cheng@outlook.com>
2017-04-25 15:24:59 -0700

    gain ratio

--- xcheng11-hw5/split.py
+++ xcheng11-hw5/split.py
@@ -9,11 +9,16 @@ def split(dataset, size):
     features = header[0:len(header)-1]
     # find split feature
     information = np.zeros(len(header) - 1)
+    split_information = np.zeros(len(header) - 1)
     for i in range(len(features)):
-        information[i] = info_gain(dataset, label, features[i])
+        info, split_info = info_gain(dataset, label, features[i])
+        information[i] = info
+        split_information[i] = split_info
     # choose the feature with lowest infomation as current tree node
-    name = features[np.argmin(information)]
-    cdd = 0
+    gain = ent - information
+    print features[np.argmax(gain)]
+    gain_ratio = gain/split_information
+    print features[np.argmax(gain_ratio)]
 
 def info_gain(dataset, label, feature):
     size = dataset.shape[0]
@@ -21,13 +26,15 @@ def info_gain(dataset, label, feature):
     groups = dataset.groupby(feature)
     counts = groups.size().reset_index(name='count')
     group_probability = np.array(counts['count'], dtype=float)/size
+    log_pr = np.log2(group_probability)
     # calculate entropy of each distinct value
     distinct_names = dataset[feature].unique()
     eps = np.zeros(len(distinct_names))
     for i in range(len(distinct_names)):
         eps[i] = entropy(groups.get_group(distinct_names[i]), label)
     info = np.dot(group_probability, eps)
-    return info
+    split_info = -np.dot(group_probability, log_pr)
+    return info, split_info
 
 # calculate entropy
 def entropy(group, label):
