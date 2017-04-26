78a031a0c67cb2c06e1920ba4f3aa3c32ab4b437
freddie <x_cheng@outlook.com>
2017-04-25 19:59:12 -0700

    witout libary

--- xcheng11-hw5/split.py
+++ xcheng11-hw5/split.py
@@ -1,60 +1,72 @@
-import pandas as pd
-import numpy as np
 from sys import stdin
+from math import log
 
-def split(dataset, size):
-    header = list(dataset)
-    label = header[-1]
-    ent = entropy(dataset, label)
-    features = header[0:len(header)-1]
-    # find split feature
-    information = np.zeros(len(header) - 1)
-    split_information = np.zeros(len(header) - 1)
-    for i in range(len(features)):
-        info, split_info = info_gain(dataset, label, features[i])
-        information[i] = info
-        split_information[i] = split_info
-    # choose the feature with lowest infomation as current tree node
-    gain = ent - information
-    print features[np.argmax(gain)]
-    gain_ratio = gain/split_information
-    print features[np.argmax(gain_ratio)]
+def split(dataset, size, header):
+    label = len(dataset[0]) - 1
+    prs = probability(dataset, label)
+    entr = entropy(prs)
+    info_gains = []
+    gain_ratio = []
+    for i in range(label):
+        infogain, splitinfo = info_gain(dataset, i, entr)
+        info_gains.append(infogain)
+        gain_ratio.append(infogain/splitinfo)
+    print header[info_gains.index(max(info_gains))]
+    print header[gain_ratio.index(max(gain_ratio))]
 
-def info_gain(dataset, label, feature):
-    size = dataset.shape[0]
-    # calculate the probability of each distinct value of this feature
-    groups = dataset.groupby(feature)
-    counts = groups.size().reset_index(name='count')
-    group_probability = np.array(counts['count'], dtype=float)/size
-    log_pr = np.log2(group_probability)
-    # calculate entropy of each distinct value
-    distinct_names = dataset[feature].unique()
-    eps = np.zeros(len(distinct_names))
-    for i in range(len(distinct_names)):
-        eps[i] = entropy(groups.get_group(distinct_names[i]), label)
-    info = np.dot(group_probability, eps)
-    split_info = -np.dot(group_probability, log_pr)
-    return info, split_info
+def info_gain(dataset, idx, all_info):
+    size = len(dataset)
+    label = len(dataset[0]) - 1
+    groups = []
+    features = {}
+    for i in range(size):
+        if dataset[i][idx] in features:
+            features[dataset[i][idx]][1] += 1
+            groups[features[dataset[i][idx]][0]].append(dataset[i])
+        else:
+            features[dataset[i][idx]] = [len(groups), 1]
+            groups.append([dataset[i]])
+    ens = []
+    prs = []
+    for i in range(len(groups)):
+        pr = probability(groups[i], label)
+        ens.append(entropy(pr))
+        prs.append(float(len(groups[i]))/size)
+    info = 0
+    splitinfo = 0
+    for i in range(len(prs)):
+        info += prs[i]*ens[i]
+        splitinfo += prs[i]*log(prs[i], 2)
+    return all_info - info, -splitinfo
+
+def probability(group, idx):
+    lables = {}
+    size = len(group)
+    for t in group:
+        if t[idx] in lables:
+            lables[t[idx]] += 1
+        else:
+            lables[t[idx]] = 1
+    counts = lables.values()
+    prs = []
+    for c in counts:
+        prs.append(float(c)/size)
+    return prs
 
 # calculate entropy
-def entropy(group, label):
-    size = group.shape[0]
-    groups = group.groupby(label).size().reset_index(name='count')
-    counts = np.array(groups['count'], dtype=float)
-    probabilities = counts/size
-    log_probabilities = -np.log2(probabilities)
-    ent = np.dot(probabilities, log_probabilities)
-    return ent
+def entropy(probability):
+    ent = 0
+    for p in probability:
+        ent += p*log(p, 2)
+    return -ent
 
 def main():
     size = input() - 1
     header = stdin.readline().split(',')
-    dataset = pd.DataFrame([], columns=header)
-    itr = 0
+    dataset = []
     for line in stdin.readlines():
-        dataset.loc[itr] = line.split(',')
-        itr += 1
-    split(dataset, size)
+        dataset.append(line.rstrip().split(','))
+    split(dataset, size, header)
     
 if __name__ == "__main__":
     main()
